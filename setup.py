890030b3dd0c58ded4119ef1bc9edc7ffc47c296
Libo Su <l.su@ixp-duesseldorf.de>
2021-11-28 16:48:53 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# ! Do not manually invoke this setup.py. Use catkin instead
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+
+setup_args = generate_distutils_setup(
+    packages=["video_recorder"],
+    package_dir={"video_recorder": "src/video_recorder"}
+)
+
+setup(**setup_args)
