7be39b4d71090c7b13f2cf2fcbc8c27ac766f1c1
Libo Su <l.su@ixp-duesseldorf.de>
2022-03-08 18:10:31 +0100

    associated saved file with participant id

--- ros/src/video_recorder/video_recorder.py
+++ ros/src/video_recorder/video_recorder.py
@@ -1,7 +1,7 @@
 import cv2
 import datetime
 from pathlib import Path
-
+import rospy
 
 class VideoRecorder:
     video_type_code = {
@@ -52,14 +52,28 @@ class VideoRecorder:
         if self._is_recording:
             raise RuntimeError("Video is alrady being recorded")
 
-        if not Path(self._out_directory).is_dir():
-            Path(self._out_directory).mkdir()
+        # parameter set by migrave_games
+        parameter_name = "/migrave/game_performance/participant_id"
+        if rospy.has_param(parameter_name):
+            participant_id = rospy.get_param(parameter_name)
+        if participant_id == "":
+            rospy.loginfo("Participant ID not set!")
+            participant_id = "ID_Unknown"
+
+        self._out_directory_id = Path(self._out_directory) / participant_id
+        if not Path(self._out_directory_id).is_dir():
+            Path(self._out_directory_id).mkdir(parents=True, exist_ok=True)
         if out_file_name is None:
             ext = self._video_type
-            file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            now = datetime.datetime.now()
+            # time in YYYY-MM-DD_HH_MM-SS format
+            time = now.strftime("%Y-%m-%d_%H-%M-%S")
+            # unixtimestamp 16 digits
+            stamp = int(datetime.datetime.timestamp(now) * 1000000)
+            file_name = f"{time}_{stamp}"
             out_file_name = f"{file_name}.{ext}"
 
-        out_file_path = Path(self._out_directory) / out_file_name
+        out_file_path = Path(self._out_directory_id) / out_file_name
         out_file_path = str(out_file_path)
         self._video_writer = cv2.VideoWriter(
             out_file_path,
