0921167ff41b6868772c4866e36f5b17afd4ec54
Intelwi <mlk04@wp.pl>
2022-03-21 15:59:56 +0100

    Removal of audio recorder

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 
 setup_args = generate_distutils_setup(
-    packages=["video_recorder", "audio_recorder"],
-    package_dir={"video_recorder": "src/video_recorder",
-                 "audio_recorder": "src/audio_recorder"}
+    packages=["video_recorder"],
+    package_dir={"video_recorder": "ros/src/video_recorder"}
 )
 
 setup(**setup_args)
